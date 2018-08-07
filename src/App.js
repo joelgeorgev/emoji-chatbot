75dd21571d947cffe0b8a2a768cba1f64736338a
Joel George V <joel.gv3@gmail.com>
2018-08-07 16:41:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import 'tachyons/css/tachyons.min.css';
 
+import github from './assets/github.svg';
+
 export class App extends React.PureComponent {
   render() {
     return (
-      <div className='tc b'>
-        Emoji Chatbot
+      <div className='flex flex-column w-80 mw8 vh-100 center pv4'>
+        <div className='flex flex-auto flex-column'>
+          <div className='b tc'>Emoji Chatbot</div>
+        </div>
+        <div className='self-center'>
+          <a href='https://github.com/joelgeorgev/emoji-chatbot'>
+            <img src={github} alt='GitHub' />
+          </a>
+        </div>
       </div>
     );
   }
