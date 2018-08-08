e7d91c3997d537beba4505cbc22210ac6b705d41
Joel George V <joel.gv3@gmail.com>
2018-08-09 00:10:58 +0530

    Add placeholder, adjust margin and padding

--- src/components/compose-message.js
+++ src/components/compose-message.js
@@ -22,11 +22,12 @@ export class ComposeMessage extends React.PureComponent {
 
   render() {
     return (
-      <div className='self-center ma2'>
+      <div className='self-center ma4'>
         <input
           type='text'
+          placeholder='Write a message'
           value={this.state.message}
-          className='ba b--light-silver'
+          className='ba b--light-silver pa2'
           onChange={this.handleChange}
           onKeyPress={this.handleKeyPress} />
       </div>
