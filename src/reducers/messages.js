c44baf5892935ddb3a6386c8bb15ec742d4d223d
Joel George V <joel.gv3@gmail.com>
2018-08-09 00:16:17 +0530

    Use ES6 syntax

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -2,7 +2,7 @@ export const messages = (state = [], action) => {
   switch (action.type) {
     case 'SEND_MESSAGE':
       const message = action.message
-      return state.concat({ author: 'You', message }, { author: 'Bot', message })
+      return [...state, { author: 'You', message }, { author: 'Bot', message }]
     default:
       return state
   }
