11d337ce9199674691ad7a8f183a7cd1a452f2a8
Joel George V <joel.gv3@gmail.com>
2018-08-16 00:32:19 +0530

    Scroll to last message on send

--- src/components/message-stream.js
+++ src/components/message-stream.js
@@ -3,10 +3,24 @@ import React from 'react'
 import { MessageBox } from '.'
 
 export class MessageStream extends React.PureComponent {
+
+  ref
+
+  setRef = (el) => {
+    this.ref = el
+  }
+
+  componentDidUpdate() {
+    const ref = this.ref
+    const { scrollHeight, clientHeight } = ref
+    const maxScrollTop = scrollHeight - clientHeight
+    ref.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0
+  }
+
   render() {
     const { messages } = this.props
     return (
-      <div className='ba br2 pa2 overflow-auto'>
+      <div className='ba br2 pa2 overflow-auto' ref={this.setRef}>
         <div className='flex flex-column ma2 min-h24'>
           {messages.map((message, index) => {
             return <MessageBox key={index} message={message} />
--- src/sagas.js
+++ src/sagas.js
@@ -1,4 +1,5 @@
 import { takeEvery, put } from 'redux-saga/effects'
+
 import translate from './moji-translate'
 
 function* processMessage(actionObj) {
