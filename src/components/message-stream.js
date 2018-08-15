7c34bdf4d09ffc6a99c4e490f6ea6409083774b1
Joel George V <joel.gv3@gmail.com>
2018-08-15 16:24:01 +0530

    Fix scroll bug

--- src/components/message-stream.js
+++ src/components/message-stream.js
@@ -7,7 +7,7 @@ export class MessageStream extends React.PureComponent {
     const { messages } = this.props
     return (
       <div className='ba br2 pa2 overflow-auto'>
-        <div className='flex flex-column ma2 h24'>
+        <div className='flex flex-column ma2 min-h24'>
           {messages.map((message, index) => {
             return <MessageBox key={index} message={message} />
           })}
