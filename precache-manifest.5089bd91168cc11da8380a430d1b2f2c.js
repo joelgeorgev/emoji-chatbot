ad1ac82a5de96e2db407b06b7906529828ff42b4
Joel George V <joel.gv3@gmail.com>
2019-01-30 00:47:07 +0000

    Updates

---
+++ precache-manifest.5089bd91168cc11da8380a430d1b2f2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d3b2fe899ef482e7348",
+    "url": "/emoji-chatbot/static/css/main.596fcb08.chunk.css"
+  },
+  {
+    "revision": "9d3b2fe899ef482e7348",
+    "url": "/emoji-chatbot/static/js/main.9d3b2fe8.chunk.js"
+  },
+  {
+    "revision": "ce8e520afb1e4d4fd8e1",
+    "url": "/emoji-chatbot/static/css/1.e5ba2196.chunk.css"
+  },
+  {
+    "revision": "ce8e520afb1e4d4fd8e1",
+    "url": "/emoji-chatbot/static/js/1.ce8e520a.chunk.js"
+  },
+  {
+    "revision": "d261644454f517383056",
+    "url": "/emoji-chatbot/static/js/runtime~main.d2616444.js"
+  },
+  {
+    "revision": "dc6635a4b3f40c69ee44a80625b8fcdc",
+    "url": "/emoji-chatbot/static/media/github.dc6635a4.svg"
+  },
+  {
+    "revision": "76d8cbb0496cb184eff868152b67ad45",
+    "url": "/emoji-chatbot/static/media/source-sans-pro-v11-latin-regular.76d8cbb0.woff2"
+  },
+  {
+    "revision": "f94f84eb867a3e3d65da0c5cad065437",
+    "url": "/emoji-chatbot/static/media/source-sans-pro-v11-latin-regular.f94f84eb.woff"
+  },
+  {
+    "revision": "e3c1ccce800d3eee13a5e10946264524",
+    "url": "/emoji-chatbot/index.html"
+  }
+];
