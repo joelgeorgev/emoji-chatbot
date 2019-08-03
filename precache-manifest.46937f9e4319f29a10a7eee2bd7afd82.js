36bf36c0f6140224e67b2371cac0f41d5aecb16b
Joel George V <joel.gv3@gmail.com>
2019-08-03 23:38:51 +0100

    Updates

---
+++ precache-manifest.46937f9e4319f29a10a7eee2bd7afd82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f148bec87637e1e5504567f0b04d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc46bddee38a7edf9db3",
+    "url": "/static/css/2.1688eb18.chunk.css"
+  },
+  {
+    "revision": "21169b5767751f18e606",
+    "url": "/static/css/main.e5372e72.chunk.css"
+  },
+  {
+    "revision": "dc46bddee38a7edf9db3",
+    "url": "/static/js/2.85c2664d.chunk.js"
+  },
+  {
+    "revision": "21169b5767751f18e606",
+    "url": "/static/js/main.088d0026.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc6635a4b3f40c69ee44a80625b8fcdc",
+    "url": "/static/media/github.dc6635a4.svg"
+  },
+  {
+    "revision": "76d8cbb0496cb184eff868152b67ad45",
+    "url": "/static/media/source-sans-pro-v11-latin-regular.76d8cbb0.woff2"
+  },
+  {
+    "revision": "f94f84eb867a3e3d65da0c5cad065437",
+    "url": "/static/media/source-sans-pro-v11-latin-regular.f94f84eb.woff"
+  }
+]);
