32305c6299e61b660b42db06503e1ac0525a9cae
Joel George V <joel.gv3@gmail.com>
2019-08-04 01:21:09 +0100

    Use concise body for arrow functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,13 @@
 import { SEND_MESSAGE, SAVE_MESSAGE } from '../constants/ActionTypes'
 
 // UI actions
-export const sendMessage = (message) => {
-  return { type: SEND_MESSAGE, message }
-}
+export const sendMessage = (message) => ({
+  type: SEND_MESSAGE,
+  message
+})
 
 // Saga actions
-export const saveMessage = (payload) => {
-  return { type: SAVE_MESSAGE, payload }
-}
+export const saveMessage = (payload) => ({
+  type: SAVE_MESSAGE,
+  payload
+})
