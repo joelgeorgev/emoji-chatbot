e3f28029be4c28ac8cb1efc18fbbe5469c1e880b
Joel George V <joel.gv3@gmail.com>
2019-09-28 21:04:14 +0100

    Add more unit tests for ComposeMessage

--- src/components/ComposeMessage/ComposeMessage.test.js
+++ src/components/ComposeMessage/ComposeMessage.test.js
@@ -4,20 +4,77 @@ import { render, fireEvent } from '@testing-library/react'
 import { ComposeMessage } from '.'
 
 describe('ComposeMessage', () => {
+  const userMessage = 'Hello World'
+
+  const arrange = () => {
+    const onSendMessage = jest.fn()
+    const { getByPlaceholderText } = render(
+      <ComposeMessage onSendMessage={onSendMessage} />
+    )
+    const inputElement = getByPlaceholderText('Write a message')
+
+    return { onSendMessage, inputElement }
+  }
+
+  const simulateMessageSubmitByUser = ({
+    inputElement,
+    message = userMessage,
+    shouldPressEnterKey = true
+  }) => {
+    fireEvent.change(inputElement, { target: { value: message } })
+
+    if (shouldPressEnterKey) {
+      fireEvent.keyPress(inputElement, { key: 'Enter', keyCode: 13 })
+    } else {
+      fireEvent.keyPress(inputElement, { key: 'J', code: 74, charCode: 74 })
+    }
+  }
+
   describe('When user types a message and presses enter key', () => {
     test('should invoke callback function', () => {
-      const onSendMessage = jest.fn()
-      const userMessage = 'Hello World'
-      const { getByPlaceholderText } = render(
-        <ComposeMessage onSendMessage={onSendMessage} />
-      )
-      const inputElement = getByPlaceholderText('Write a message')
+      const { onSendMessage, inputElement } = arrange()
 
-      fireEvent.change(inputElement, { target: { value: userMessage } })
-      fireEvent.keyPress(inputElement, { key: 'Enter', keyCode: 13 })
+      simulateMessageSubmitByUser({ inputElement })
 
       expect(onSendMessage).toHaveBeenCalledTimes(1)
       expect(onSendMessage).toHaveBeenCalledWith(userMessage)
+    })
+  })
+
+  describe('When user types an empty message and presses enter key', () => {
+    test('should NOT invoke callback function', () => {
+      const { onSendMessage, inputElement } = arrange()
+
+      simulateMessageSubmitByUser({ inputElement, message: '' })
+
+      expect(onSendMessage).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('When user types a message with extra spaces and presses enter key', () => {
+    test('should invoke callback function with the trimmed message', () => {
+      const { onSendMessage, inputElement } = arrange()
+
+      simulateMessageSubmitByUser({
+        inputElement,
+        message: `     ${userMessage}     `
+      })
+
+      expect(onSendMessage).toHaveBeenCalledTimes(1)
+      expect(onSendMessage).toHaveBeenCalledWith(userMessage)
+    })
+  })
+
+  describe('When user types a message and presses any other key', () => {
+    test('should NOT invoke callback function', () => {
+      const { onSendMessage, inputElement } = arrange()
+
+      simulateMessageSubmitByUser({
+        inputElement,
+        shouldPressEnterKey: false
+      })
+
+      expect(onSendMessage).toHaveBeenCalledTimes(0)
     })
   })
 })
