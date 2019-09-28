da4145d786f8065e62f0acadd85bf1049950602d
Joel George V <joel.gv3@gmail.com>
2019-09-28 15:21:17 +0100

    Add unit test for MessageBox

---
+++ src/components/MessageBox/MessageBox.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import { MessageBox } from '.'
+
+describe('MessageBox', () => {
+  describe('When given a message object', () => {
+    const getDefaultProps = () => ({ message: { author: '', text: '' } })
+    const createProps = (props) => ({ ...getDefaultProps(), ...props })
+
+    test('should render the message', () => {
+      const text = 'Hello World'
+      const { getByText } = render(
+        <MessageBox {...createProps({ message: { text } })} />
+      )
+
+      expect(getByText(text)).toBeDefined()
+    })
+  })
+})
