5b1487c075392f97d2390268465a92c9206084fe
Joel George V <joel.gv3@gmail.com>
2019-09-30 00:13:09 +0100

    Add unit tests for ChatWindow

---
+++ src/components/ChatWindow/ChatWindow.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { ChatWindow } from '.'
+
+describe('ChatWindow', () => {
+  const getDefaultProps = () => ({ messages: [], onSendMessage: jest.fn() })
+  const createProps = (props) => ({ ...getDefaultProps(), ...props })
+
+  describe('When given an array of messages', () => {
+    test('should render the messages', () => {
+      const text = 'Hello World'
+      const { getByText } = render(
+        <ChatWindow {...createProps({ messages: [{ text }] })} />
+      )
+
+      expect(getByText(text)).toBeDefined()
+    })
+  })
+
+  describe('When given a callback function', () => {
+    describe('When user enters a message', () => {
+      test('should invoke callback function', () => {
+        const onSendMessage = jest.fn()
+        const userMessage = 'Hello World'
+        const { getByPlaceholderText } = render(
+          <ChatWindow {...createProps({ onSendMessage })} />
+        )
+        const inputElement = getByPlaceholderText('Write a message')
+
+        fireEvent.change(inputElement, { target: { value: userMessage } })
+        fireEvent.keyPress(inputElement, { key: 'Enter', keyCode: 13 })
+
+        expect(onSendMessage).toHaveBeenCalledTimes(1)
+        expect(onSendMessage).toHaveBeenCalledWith(userMessage)
+      })
+    })
+  })
+})
