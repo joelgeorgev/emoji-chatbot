965145686d357a12e91925f12778eede3b791ac5
Joel George V <joel.gv3@gmail.com>
2019-09-30 00:37:40 +0100

    Update import statement in selectors test

--- src/reducers/selectors.test.js
+++ src/reducers/selectors.test.js
@@ -1,8 +1,8 @@
-import { getMessages } from '.'
+import * as selectors from '.'
 
 describe('Selectors', () => {
   test('getMessages should select messages from state', () => {
     const messages = [{ text: 'Hello World' }]
-    expect(getMessages({ messages })).toEqual(messages)
+    expect(selectors.getMessages({ messages })).toEqual(messages)
   })
 })
