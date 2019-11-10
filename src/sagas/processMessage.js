e9ee31b21cf5f477fa0f32a9e19c6e5d1e5dddc4
Joel George V <joel.gv3@gmail.com>
2019-11-10 23:54:59 +0000

    Destructure action object

--- src/sagas/processMessage.js
+++ src/sagas/processMessage.js
@@ -3,9 +3,7 @@ import { put } from 'redux-saga/effects'
 import translate from '../moji-translate'
 import { saveMessage } from '../actions'
 
-export const processMessage = function*(action) {
-  yield put(saveMessage({ author: 'You', text: action.text }))
-  yield put(
-    saveMessage({ author: 'Bot', text: translate.translate(action.text) })
-  )
+export const processMessage = function*({ text }) {
+  yield put(saveMessage({ author: 'You', text }))
+  yield put(saveMessage({ author: 'Bot', text: translate.translate(text) }))
 }
