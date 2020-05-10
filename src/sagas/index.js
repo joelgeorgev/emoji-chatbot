24f05777323da193f02783a2c83d802b32614aec
Joel George V <joel.gv3@gmail.com>
2020-05-10 17:48:21 +0100

    Upgrade prettier

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,6 +3,6 @@ import { all, takeEvery } from 'redux-saga/effects'
 import { SEND_MESSAGE } from '../actions'
 import { translateMessage } from './translateMessage'
 
-export const rootSaga = function*() {
+export const rootSaga = function* () {
   yield all([takeEvery(SEND_MESSAGE, translateMessage)])
 }
--- src/sagas/translateMessage.js
+++ src/sagas/translateMessage.js
@@ -3,6 +3,6 @@ import { put } from 'redux-saga/effects'
 import translate from '../moji-translate'
 import { messageTranslated } from '../actions'
 
-export const translateMessage = function*({ payload: { text } }) {
+export const translateMessage = function* ({ payload: { text } }) {
   yield put(messageTranslated(translate.translate(text)))
 }
