7f2a05eb450043d00eeb86fe49033e8548e01594
Joel George V <joel.gv3@gmail.com>
2020-05-31 16:27:54 +0100

    Update format of reducer tests

--- src/reducers/messages.test.js
+++ src/reducers/messages.test.js
@@ -4,26 +4,19 @@ import { sendMessage, messageTranslated } from '../actions'
 const text = 'Hello'
 
 describe('messages reducer', () => {
-  describe('Given NO action', () => {
-    const action = {}
-    let newState
-
-    beforeEach(() => {
-      newState = messages(undefined, action)
-    })
-
-    test('returns initial state', () => {
-      expect(newState).toEqual([
-        {
-          author: 'Bot',
-          text: 'Write a message and see it translated to emojis!'
-        }
-      ])
-    })
+  test('returns the initial state', () => {
+    expect(messages(undefined, {})).toEqual([
+      {
+        author: 'Bot',
+        text: 'Write a message and see it translated to emojis!'
+      }
+    ])
   })
 
   test('handles SEND_MESSAGE action', () => {
-    expect(messages([], sendMessage(text))).toEqual([
+    const action = sendMessage(text)
+
+    expect(messages([], action)).toEqual([
       {
         author: 'You',
         text
@@ -32,7 +25,9 @@ describe('messages reducer', () => {
   })
 
   test('handles MESSAGE_TRANSLATED action', () => {
-    expect(messages([], messageTranslated(text))).toEqual([
+    const action = messageTranslated(text)
+
+    expect(messages([], action)).toEqual([
       {
         author: 'Bot',
         text
