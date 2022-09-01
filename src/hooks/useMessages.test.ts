7cf1093672cd51c5fec5e5b2833361a904892c1f
Joel George V <joel.gv3@gmail.com>
2022-09-01 15:46:47 +0100

    Bump dependencies

--- src/hooks/useMessages.test.ts
+++ src/hooks/useMessages.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 
 import { useMessages } from './useMessages'
 import { translate } from '../moji-translate'
